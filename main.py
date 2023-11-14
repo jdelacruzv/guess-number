2be737b1da757215ffddc61f88cedf279f930352
Jose De La Cruz <jldlcv@gmail.com>
2023-11-14 22:43:00 +0100

    The exit statement was added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 
 def title_bar():
@@ -7,20 +8,34 @@ def title_bar():
 	print(line)
 	print("{:^60}".format(message))
 	print(line)
-	print("El objetivo es adivinar el número generado por el ordenador.\n")
+	print("El objetivo es adivinar el número generado por el ordenador.")
+	print("Para terminar el programa digite salir\n")
 
 
 def guess_number():
-	number = int(input("Ingrese el rango final del número: "))
-	random_number = random.randint(1, number)
-	user_prediction = 0
-	while user_prediction != random_number:
-		user_prediction = int(input(f"Adivina el número entre 1 y {number}: "))
-		if user_prediction < random_number:
-			print("Intenta otra vez. El número es muy bajo")
-		elif user_prediction > random_number:
-			print("Intenta otra vez. El número es muy alto")
-	print(f"\n¡Felicitaciones! Adivinastes el número {random_number} correctamente.\n")
+	while True:
+		number = input("Ingrese rango final del número: ")	
+		if number == 'salir':
+			break
+		try:
+			random_number = random.randint(1, int(number))
+			user_prediction = 0
+			while user_prediction != random_number:
+				user_prediction = input(f"Adivina el número entre 1 y {number}: ")
+				if user_prediction == "salir":
+					os._exit(0)
+				try:
+					user_prediction = int(user_prediction)
+					if user_prediction < random_number:
+						print("Intenta otra vez, el número es muy bajo")
+					elif user_prediction > random_number:
+						print("Intenta otra vez, el número es muy alto")
+				except:
+					print('Entrada inválida...')
+			print(f"\n¡Felicitaciones! Adivinastes el número {random_number} correctamente.\n")
+			break
+		except:
+			print('Entrada inválida...')
 
 
 def main():
